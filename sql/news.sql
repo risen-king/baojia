f21fb79ae35fea331d3178ddd129af0980323e8b
ahcj <ahcj@163.com>
2017-08-21 11:19:49 +0800

    生成 News crud

---
+++ sql/news.sql
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  ahcj
+ * Created: 2017-8-20
+ */
+
+create table if not exists `news`(
+        `id`    int    unsigned auto_increment,
+        `title`     varchar(50)     not null,
+        `content`   text,
+        `created_at`  timestamp,
+        `updated_at`  timestamp,
+     
+          primary key (`id`)
+ 
+)engine=innodb default charset=utf8
